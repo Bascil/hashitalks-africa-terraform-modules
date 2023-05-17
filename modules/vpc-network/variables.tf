e9a235e1ff0855df731729e8cb08291bd7c93fb4
Basil Ndonga <basil.ndonga@iprocu.re>
2023-05-17 18:51:40 +0300

    updated vpc network

--- modules/vpc-network/variables.tf
+++ modules/vpc-network/variables.tf
@@ -7,3 +7,16 @@ variable "region" {
   description = "The region for subnetworks in the network"
   type        = string
 }
+
+variable vpc_name {
+  description = "The name of vpc network"
+  type        = string
+}
+
+variable vpc_subnet {
+  description = "The name of vpc sub network"
+  type        = string
+}
+
+variable vpc_cidr {}
+variable ip_ranges {}
