64870bb35a41278237b2ba2205839947ac04f3ec
Basil Ndonga <basil.ndonga@iprocu.re>
2023-05-17 21:49:38 +0300

    updated cluster modules

--- modules/cluster-secrets/variables.tf
+++ modules/cluster-secrets/variables.tf
@@ -8,6 +8,11 @@ variable "secret_name" {
   description = "The name of cluster secret"
 }
 
+variable "branch" {
+  type        = string
+  description = "The name of branch"
+}
+
 variable "secrets" {
   description = "A map of secrets"
 }
