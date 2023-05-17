513c53aa74f683f9d7eaffc1207e8c8488ac9805
Basil Ndonga <basil.ndonga@iprocu.re>
2023-05-18 00:58:04 +0300

    updated cloud build module

--- modules/cloud-build/outputs.tf
+++ modules/cloud-build/outputs.tf
@@ -1,10 +1,10 @@
-output "trigger_name" {
-    description = "Name of the trigger"
-    value = google_cloudbuild_trigger.trigger.name
-}
+# output "trigger_name" {
+#     description = "Name of the trigger"
+#     value = google_cloudbuild_trigger.trigger.name
+# }
 
-output "file_name" {
-    description = "Name of the file"
-    value = google_cloudbuild_trigger.trigger.filename
-}
+# output "file_name" {
+#     description = "Name of the file"
+#     value = google_cloudbuild_trigger.trigger.filename
+# }
 
