c1d9e8f9f04fc4d64f0c94622c8d236dac2ba103
Basil Ndonga <basil.ndonga@iprocu.re>
2023-05-18 12:50:39 +0300

    removed cluster module

--- modules/cluster-secrets/main.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_secret" "cluster_secret" {
-  metadata {
-    name      = "${var.secret_name}-cluster-secret"
-    namespace = var.branch
-    labels    = {}
-  }
-
-  data = var.secrets
-}
